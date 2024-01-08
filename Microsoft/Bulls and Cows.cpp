54fa3adae12bbbe4c1101ab149e97ed1bb00ce6d
sunilsk17 <118009053+sunilsk17@users.noreply.github.com>
2024-01-08 20:49:01 +0530

    Create Bulls and Cows.cpp

---
+++ Microsoft/Bulls and Cows.cpp
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/bulls-and-cows/description/ */
+//Solution - Bulls and Cows
+class Solution {
+public:
+    string getHint(string& secret, string &guess) {
+        unordered_map<char,int> secret_freq;
+        unordered_map<char,int> guess_freq;
+
+        int size = secret.size();
+
+        int bulls = 0;
+        int cows = 0;
+        for(int i = 0; i < size; i++){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }else{
+                secret_freq[secret[i]]++;
+                guess_freq[guess[i]]++;
+            }
+        }
+        for(auto it: guess_freq){
+            cows += min(it.second,secret_freq[it.first]);
+        }
+        return to_string(bulls)+"A"+to_string(cows)+"B";
+    }
+};
