2c12201918a6dff54a7eba0d59331d0dd0c25261
sunilsk17 <118009053+sunilsk17@users.noreply.github.com>
2024-01-10 19:47:17 +0530

    Create Counting Nice Sub Arrays.cpp

---
+++ Microsoft/Counting Nice Sub Arrays.cpp
@@ -0,0 +1,25 @@
+/* https://leetcode.com/problems/count-number-of-nice-subarrays/description/ */
+// SOlution - Count Number of Nice Subarrays
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size(), dis = 1, i = 0, cnt = 0, ans = 0;
+        vector<int> d(n + 1, 1);
+
+        for(int i=n-1; i>=0; i--) {
+            nums[i] % 2 ? dis = 1 : dis++;
+            d[i] = dis;
+        }
+       
+        for(int j=0; j<n; j++) {
+            if(nums[j] % 2) cnt++;
+
+            while(i <= j && cnt == k) {
+                ans += d[j + 1];
+                if(nums[i] % 2) cnt--;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
