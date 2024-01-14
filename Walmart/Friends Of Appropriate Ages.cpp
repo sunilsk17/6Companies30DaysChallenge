b063d577e9efb77c971e97b1dcc4e7aca4ffb766
sunilsk17 <118009053+sunilsk17@users.noreply.github.com>
2024-01-14 20:46:57 +0530

    Create Friends Of Appropriate Ages.cpp

---
+++ Walmart/Friends Of Appropriate Ages.cpp
@@ -0,0 +1,23 @@
+/* https://leetcode.com/problems/friends-of-appropriate-ages/description/ */
+//Soultion - Friends Of Appropriate Ages
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        int ans = 0;
+        sort(ages.begin(), ages.end());
+        
+        
+        unordered_map<int,int> mp;
+        for(int i = 0; i < ages.size(); i++) {
+            if(!mp.count(ages[i])) mp[ages[i]] = i;
+        }
+        
+        
+        for(int i = 0; i < ages.size(); i++) {
+            int x= lower_bound(ages.begin(), ages.end(), 2*(ages[i]-7)) - ages.begin();
+            if(x <= i) continue;
+            ans += x-mp[ages[i]]-1; 
+        }
+        return ans;
+    }
+};
