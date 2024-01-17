952ab10eed0c2857a44c6e8e325fd6b224f79475
sunilsk17 <118009053+sunilsk17@users.noreply.github.com>
2024-01-17 21:30:46 +0530

    Create Short Encoding of Words.cpp

---
+++ Adobe/Short Encoding of Words.cpp
@@ -0,0 +1,21 @@
+/* https://leetcode.com/problems/short-encoding-of-words/description/ */
+//Solution - Short Encoding of Words
+class Solution 
+{
+public:
+    int minimumLengthEncoding(vector<string>& words) 
+    {
+        set<string> s(words.begin(), words.end()); 
+        for(auto &word: words)
+        {
+            for(int i=1; i<word.size(); i++) 
+                s.erase(word.substr(i));
+        }
+        
+        int ans=0;
+        for(auto word:s)
+            ans += word.size()+1;
+        
+        return ans;
+    }
+};
